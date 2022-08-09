a1697b5dab3fe61430159765606024cf1d31a127
steberman <steberman@posteo.net>
2022-08-09 18:07:47 +0200

    End of chapter 5

--- cdk/src/main/java/com/myorg/CdkApp.java
+++ cdk/src/main/java/com/myorg/CdkApp.java
@@ -1,40 +1,43 @@
 package com.myorg;
 
+import static java.util.Objects.requireNonNull;
+
+import dev.stratospheric.cdk.SpringBootApplicationStack;
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.Environment;
 
 
 public class CdkApp {
-    public static void main(final String[] args) {
-        App app = new App();
 
-        new CdkStack(app, "CdkStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+        //private static final String DOCKER_IMAGE_URL = "docker.io/stebeman/todo-app-v1:latest";
+        private static final String DOCKER_IMAGE_URL = "docker.io/stratospheric/todo-app-v1:latest";
+        
+        public static void main(final String[] args) {
+                App app = new App();
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+                String accountId = (String) app
+                                .getNode()
+                                .tryGetContext("accountId");
+                requireNonNull(accountId, "context variable 'accountId' must not be null");
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
+                String region = (String) app
+                                .getNode()
+                                .tryGetContext("region");
+                requireNonNull(region, "context variable 'region' must not be null");
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+                new SpringBootApplicationStack(
+                                app,
+                                "SpringBootApplication",
+                                makeEnv(accountId, region),
+                                DOCKER_IMAGE_URL);
 
-        app.synth();
-    }
+                app.synth();
+        }
+
+        private static Environment makeEnv(String accountId, String region) {
+                return Environment.builder()
+                                .account(accountId)
+                                .region(region)
+                                .build();
+        }
 }
-
