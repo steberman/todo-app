93dc9a1c4ed54769d070d22a7d8a08ec39d89485
steberman <steberman@posteo.net>
2022-08-15 17:34:13 +0200

    Initial version of todo Springboot application

---
+++ application/src/main/java/dev/steberman/todo/IndexController.java
@@ -0,0 +1,18 @@
+package dev.steberman.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+  
+
+  @GetMapping
+  @RequestMapping("/")
+  public String getIndex() {
+    return "This is steberman's todo-app.";
+    
+  }
+}
---
+++ application/src/test/java/dev/steberman/todo/TodoApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.steberman.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// succeeds if spring successfully starts up
+	}
+
+}
