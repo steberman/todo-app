b59e68a65a335760ff43538d2567cee32e8bb2c3
steberman <steberman@posteo.net>
2022-08-24 10:06:48 +0200

    Disable integration test w/o  testcontainers

--- application/src/test/java/dev/steberman/todo/TodoApplicationTests.java
+++ application/src/test/java/dev/steberman/todo/TodoApplicationTests.java
@@ -1,14 +1,28 @@
 package dev.steberman.todo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Disabled;
+
+@Disabled("Disabled until introducing test containers")
+@ActiveProfiles("dev")
 @SpringBootTest
 class TodoApplicationTests {
 
+
+	@Autowired
+	private ApplicationContext applicationContext;
+  
 	@Test
 	void contextLoads() {
 		// succeeds if spring successfully starts up
+		assertNotNull(applicationContext);
 	}
 
 }
