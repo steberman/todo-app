0fc10326a25b62f9bd4bcb0f4fa0a462a983c02c
steberman <steberman@posteo.net>
2022-10-21 18:39:24 +0200

    First working version based on chap. 11

--- application/run-local.sh
+++ application/run-local.sh
@@ -1,2 +1,2 @@
 #./mvnw spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=dev"
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Drun.jvmArguments="-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses"
